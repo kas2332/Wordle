fea8e5307b999f1cfc1c3539c6f0a15538b49134
kas2332 <kas233210@gmail.com>
2023-05-15 06:37:50 -0500

    Added KeyListener to frame instead of Jlabel

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,6 @@
-import javax.swing.*;
 import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 
 public class Main {
-    static KeyListener listener = new KeyListener() {
-        @Override
-        public void keyTyped(KeyEvent e) {
-            printChar(e.getKeyChar());            }
-
-        @Override
-        public void keyPressed(KeyEvent e) {
-            printChar(e.getKeyChar());            }
-
-        @Override
-        public void keyReleased(KeyEvent e) {
-            printChar(e.getKeyChar());
-        }
-    };
     public static void main(String[] args) {
 //        JLabel[][] jLabels = new JLabel[5][7];
 //        for (int row = 0; row < 7; row++) {
@@ -37,7 +21,7 @@ public class Main {
         //newJPanel.jLabel1.addKeyListener(listener);
     }
 
-    public static void printChar (char ch) {
-        System.out.println(ch);
+    public static void printChar(KeyEvent e) {
+        System.out.println(e.getKeyChar());
     }
 }
