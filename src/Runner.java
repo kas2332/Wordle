d43292bb0076d2aad9883de14d84df41db14e3bf
kas2332 <ks4292@k12.sd.us>
2023-05-18 10:00:33 -0500

    IDK

--- src/Runner.java
+++ src/Runner.java
@@ -1,11 +1,28 @@
+import javax.swing.*;
 import java.awt.event.KeyEvent;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
-public class Runner {
+public class Runner{
     static WordleGUI wordleGUI;
-
+    String[] words = new String[14855];
     public static void main(String[] args) {
+        Runner runner = new Runner();
+        runner.readWordsList();
         wordleGUI = new WordleGUI();
         wordleGUI.wordleGUIMaker();
+    }
+
+    public void readWordsList () {
+        int i = 0;
+        for (; i < 14855; i++) {
+            try {
+                words[i] = Files.readAllLines(Paths.get("C:\\Users\\ks4292\\IdeaProjects\\Wordle\\src\\ValidWords.txt")).get(i);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 
     public void printChar(KeyEvent e, int row, int col) {
@@ -13,14 +30,17 @@ public class Runner {
         boolean letter = (e.getKeyCode() >= KeyEvent.VK_A && e.getKeyCode() <= KeyEvent.VK_Z);
         boolean enter = (e.getKeyCode() == KeyEvent.VK_ENTER);
 
-        if ((backspace || letter || enter) && row < 6) {
+        if (backspace || letter || enter) {
             wordleGUI.jLabels[row][col].setEditable(false);
             if (backspace) {
                 doBackspace(row, col);
             } else if (letter) {
-                doLetter(row, col);
-            } else if (isValidGuess(row)){
+                doLetter(e, row, col);
+            } else if (checkIfIsValidGuess(row)){
                 doEnter(row, col);
+            } else {
+                dispMessage("invalid guess");
+                wordleGUI.jLabels[row][col].setEditable(true);
             }
         }
     }
@@ -35,7 +55,7 @@ public class Runner {
         }
     }
     
-    public void doLetter (int row, int col) {
+    public void doLetter (KeyEvent e, int row, int col) {
         wordleGUI.jLabels[row][col].setText(String.valueOf(e.getKeyChar()).toUpperCase());
         if (col == 4) {
             wordleGUI.jLabels[row][col].setEditable(true);
@@ -49,25 +69,26 @@ public class Runner {
         wordleGUI.jLabels[row + 1][0].setEditable(true);   
     }
 
-    public boolean isValidGuess (int row) {
-        String s = "";
-        for (int i; i < 5; i++) {
-            s += wordleGUI.jLabels[row][i];
+    public boolean checkIfIsValidGuess(int row) {
+        StringBuilder s = new StringBuilder();
+        for (int i = 0; i < 5; i++) {
+            s.append(wordleGUI.jLabels[row][i].getText());
         }
-        if ((s.lenght() == 5) && (binarySearch(s) >= 0) {
-            return true;
-        } else {
-            return false;
-        }
+        System.out.println(binarySearch(s.toString().toLowerCase()));
+        return (s.length() == 5) && (binarySearch(s.toString().toLowerCase()) >= 0);
     }
     
-    static int binarySearch(String[] arr, String x)
+    public int binarySearch(String x)
     {
-        int l = 0, r = arr.length - 1;
+        int l = 0, r = words.length - 1;
         while (l <= r) {
             int m = l + (r - l) / 2;
- 
-            int res = x.compareTo(arr[m]);
+
+            System.out.println(l);
+            System.out.println(r);
+            System.out.println(m);
+            System.out.println(words[m]);
+            int res = x.compareTo(words[m]);
  
             // Check if x is present at mid
             if (res == 0)
@@ -83,4 +104,13 @@ public class Runner {
         }
         return -1;
     }
+    public void dispMessage (String message) {
+        switch (message) {
+            case "invalid guess" ->
+                    JOptionPane.showMessageDialog(null, "Error, word is not valid.", "Error", JOptionPane.ERROR_MESSAGE);
+            case "lose" ->
+                    JOptionPane.showMessageDialog(null, "Sorry, you lost.", "Error", JOptionPane.ERROR_MESSAGE);
+            case "win" ->
+                    JOptionPane.showMessageDialog(null, "Hooray, you win!", "Error", JOptionPane.ERROR_MESSAGE);
+        }            }
 }
